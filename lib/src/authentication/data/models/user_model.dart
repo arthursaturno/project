ee1c34492ae067ba3f4acffe1c76578a6ba2c21a
Arthur Henrique <arthurnam@outlook.com>
2024-06-11 03:08:17 -0300

    feat: implementing the  model user with test

--- lib/src/authentication/data/models/user_model.dart
+++ lib/src/authentication/data/models/user_model.dart
@@ -1,10 +1,54 @@
+import 'dart:convert';
+
+import 'package:project/core/utils/typedef.dart';
 import 'package:project/src/authentication/domain/entities/user.dart';
 
 class UserModel extends User {
   const UserModel({
     required super.id,
-    required super.createdAt,
     required super.name,
     required super.avatar,
+    required super.createdAt,
   });
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(jsonDecode(source) as DataMap);
+
+  UserModel.fromMap(DataMap map)
+      : this(
+          id: map['id'] as String,
+          name: map['name'] as String,
+          avatar: map['avatar'] as String,
+          createdAt: map['createdAt'] as String,
+        );
+
+  const UserModel.empty()
+      : this(
+            id: '1',
+            createdAt: '_empty.createdAt',
+            name: '_empty.name',
+            avatar: '_empty.avatar');
+
+  UserModel copyWith({
+    String? id,
+    String? name,
+    String? avatar,
+    String? createdAt,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      avatar: avatar ?? this.avatar,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+
+  DataMap toMap() => {
+        'id': id,
+        'name': name,
+        'avatar': avatar,
+        'createdAt': createdAt,
+      };
+
+  String toJson() => jsonEncode(toMap());
 }
